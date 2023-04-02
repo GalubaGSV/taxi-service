a4c682fa118fa12264189e4938c8e722f95401d0
Sergiy Golubchenko <galuba1@gmail.com>
2023-04-02 23:41:24 +0300

    Taxi service was implemented

---
+++ src/main/java/taxi/controller/manufacturer/AddManufacturerController.java
@@ -0,0 +1,29 @@
+package taxi.controller.manufacturer;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import taxi.lib.Injector;
+import taxi.model.Manufacturer;
+import taxi.service.ManufacturerService;
+
+public class AddManufacturerController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("taxi");
+    private final ManufacturerService manufacturerService = (ManufacturerService) injector
+            .getInstance(ManufacturerService.class);
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/manufacturers/add.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        manufacturerService.create(new Manufacturer(req.getParameter("name"),
+                req.getParameter("country")));
+        resp.sendRedirect(req.getContextPath() + "/manufacturers");
+    }
+}
